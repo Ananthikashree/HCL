c650eeb9796611924c6d0b6b9348333e48d599c6
ananthikashree54@gmail.com <ananthikashree54@gmail.com>
2026-04-07 09:37:09 +0530

    first commit

--- abstractclass.java
+++ abstractclass.java
@@ -1,5 +1,35 @@
+abstract class Place {
+
+   
+    abstract void famousFor();
+
+    
+    void location() {
+        System.out.println("This place is in India");
+    }
+}
+
+class Ooty extends Place {
+    void famousFor() {
+        System.out.println("Ooty is famous for hill station and tea gardens");
+    }
+}
+
+class Chennai extends Place {
+    void famousFor() {
+        System.out.println("Chennai is famous for Marina Beach and culture");
+    }
+}
+
 public class abstractclass {
     public static void main(String[] args) {
-        System.out.println("This is an example of an abstract class in Java.");
+
+        Place p1 = new Ooty();
+        p1.famousFor();
+        p1.location();
+
+        Place p2 = new Chennai();
+        p2.famousFor();
+        p2.location();
     }
 }
